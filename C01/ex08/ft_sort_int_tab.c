55d1d5f020bdee55741c03972042a95492e03d9d
Mustafa  Bülbül <157781038+MustafaBulbul0@users.noreply.github.com>
2024-08-21 03:24:36 +0300

    Add files via upload

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mubulbul <mubulbul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 02:44:35 by mubulbul          #+#    #+#             */
+/*   Updated: 2024/08/19 03:11:48 by mubulbul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	swap;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				swap = tab[i];
+				tab[i] = tab[j];
+				tab[j] = swap;
+			}
+			j++;
+		}
+		i++;
+	}
+}
